ba21e0925c531e2d082ffd7cbea2f3770168c8fb
y0n1 <y0n1@outlook.com>
2017-06-05 01:13:15 +0300

    chore: Removes open-chrome script

--- open-chrome.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-if [ "$(uname -s)" = "Darwin" ]; then
-    open -n -a /Applications/Google\ Chrome.app --args --disable-web-security --user-data-dir="/tmp/google-chrome"
-else
-    echo "Right now this script works only on MacOS" 
-fi
